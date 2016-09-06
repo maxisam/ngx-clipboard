17a4768f53c1511558d34a026f5af8f975237fec
Sam Lin <maxisam@gmail.com>
2016-09-06 18:45:57 -0500

    chore: add comment

--- demo/app/app.module.ts
+++ demo/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { CommonModule }   from '@angular/common';
-import { FormsModule }         from '@angular/forms';
+import { FormsModule }    from '@angular/forms';
 
 /* App Root */
 import { AppComponent }   from './app.component';
 
 /* Feature Modules */
 import { ClipboardModule }  from 'angular2-clipboard';
+
+/* Use local copy instead of npm version*/
 // import { ClipboardModule }  from './clipboard/clipboard.module';
 
 @NgModule({
