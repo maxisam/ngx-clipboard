370f99078ff4966c5503f4ed729b0782a7db78c0
Sam Lin <maxisam@gmail.com>
2016-09-06 18:33:50 -0500

    regenerate index.js

--- index.js
+++ index.js
@@ -90,11 +90,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	    return ClipboardModule;
 	}());
 	exports.ClipboardModule = ClipboardModule;
-	/*
-	Copyright 2016 Google Inc. All Rights Reserved.
-	Use of this source code is governed by an MIT-style license that
-	can be found in the LICENSE file at http://angular.io/license
-	*/
 
 
 /***/ },
