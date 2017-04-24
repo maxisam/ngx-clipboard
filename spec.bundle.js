98396c3839365bea3ba4fc825ec170d873e11280
Sam Lin <maxisam@gmail.com>
2017-04-24 12:17:06 -0500

    fix(test): test environment setup

--- spec.bundle.js
+++ spec.bundle.js
@@ -19,7 +19,7 @@ testing.TestBed.initTestEnvironment(
     browser.platformBrowserDynamicTesting()
 );
 
-var testContext = require.context('./tests', true, /\.spec\.ts/);
+var testContext = require.context('./src', true, /\.spec\.ts/);
 
 function requireAll(requireContext) {
     return requireContext.keys().map(requireContext);
