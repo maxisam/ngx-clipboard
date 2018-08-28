8835518b3ef309ea68b3cd14e32e52a737b19f58
Sam Lin <maxisam@gmail.com>
2018-08-28 08:50:05 -0500

    fix: add container as input property.
    the container is the one to append the tempary textarea. It is default to body if unset   #145

--- projects/ngx-clipboard/src/lib/ngx-clipboard.directive.ts
+++ projects/ngx-clipboard/src/lib/ngx-clipboard.directive.ts
@@ -1,66 +1,59 @@
-import {
-  Directive,
-  EventEmitter,
-  HostListener,
-  Input,
-  OnDestroy,
-  OnInit,
-  Output
-} from '@angular/core';
+import { Directive, EventEmitter, HostListener, Input, OnDestroy, OnInit, Output } from '@angular/core';
 
 import { ClipboardService } from './ngx-clipboard.service';
 
 @Directive({
-  // tslint:disable-next-line:directive-selector
-  selector: '[ngxClipboard]'
+    // tslint:disable-next-line:directive-selector
+    selector: '[ngxClipboard]'
 })
 export class ClipboardDirective implements OnInit, OnDestroy {
-  // tslint:disable-next-line:no-input-rename
-  @Input('ngxClipboard') public targetElm: HTMLInputElement;
+    // tslint:disable-next-line:no-input-rename
+    @Input('ngxClipboard')
+    public targetElm: HTMLInputElement;
+    @Input()
+    public container: HTMLInputElement;
 
-  @Input() public cbContent: string;
+    @Input()
+    public cbContent: string;
 
-  @Output() public cbOnSuccess: EventEmitter<any> = new EventEmitter<any>();
+    @Output()
+    public cbOnSuccess: EventEmitter<any> = new EventEmitter<any>();
 
-  @Output() public cbOnError: EventEmitter<any> = new EventEmitter<any>();
-  constructor(private clipboardSrv: ClipboardService) {}
+    @Output()
+    public cbOnError: EventEmitter<any> = new EventEmitter<any>();
+    constructor(private clipboardSrv: ClipboardService) {}
 
-  // tslint:disable-next-line:no-empty
-  public ngOnInit() {}
-
-  public ngOnDestroy() {
-    this.clipboardSrv.destroy();
-  }
-
-  @HostListener('click', ['$event.target'])
-  public onClick(event: Event) {
-    if (!this.clipboardSrv.isSupported) {
-      this.handleResult(false, undefined);
-    } else if (
-      this.targetElm &&
-      this.clipboardSrv.isTargetValid(this.targetElm)
-    ) {
-      this.handleResult(
-        this.clipboardSrv.copyFromInputElement(this.targetElm),
-        this.targetElm.value
-      );
-    } else if (this.cbContent) {
-      this.handleResult(
-        this.clipboardSrv.copyFromContent(this.cbContent),
-        this.cbContent
-      );
+    // tslint:disable-next-line:no-empty
+    public ngOnInit() {
+        if (!this.container) {
+            this.container = this.clipboardSrv.document.body;
+        }
     }
-  }
 
-  /**
-   * Fires an event based on the copy operation result.
-   * @param succeeded
-   */
-  private handleResult(succeeded: boolean, copiedContent: string | undefined) {
-    if (succeeded) {
-      this.cbOnSuccess.emit({ isSuccess: true, content: copiedContent });
-    } else {
-      this.cbOnError.emit({ isSuccess: false });
+    public ngOnDestroy() {
+        this.clipboardSrv.destroy(this.container);
     }
-  }
+
+    @HostListener('click', ['$event.target'])
+    public onClick(event: Event) {
+        if (!this.clipboardSrv.isSupported) {
+            this.handleResult(false, undefined);
+        } else if (this.targetElm && this.clipboardSrv.isTargetValid(this.targetElm)) {
+            this.handleResult(this.clipboardSrv.copyFromInputElement(this.targetElm), this.targetElm.value);
+        } else if (this.cbContent) {
+            this.handleResult(this.clipboardSrv.copyFromContent(this.cbContent, this.container), this.cbContent);
+        }
+    }
+
+    /**
+     * Fires an event based on the copy operation result.
+     * @param succeeded
+     */
+    private handleResult(succeeded: boolean, copiedContent: string | undefined) {
+        if (succeeded) {
+            this.cbOnSuccess.emit({ isSuccess: true, content: copiedContent });
+        } else {
+            this.cbOnError.emit({ isSuccess: false });
+        }
+    }
 }
