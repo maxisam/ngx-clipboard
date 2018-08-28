d544d842bfffff08e29403214b22215897897d19
Sam Lin <maxisam@gmail.com>
2018-08-28 10:26:44 -0500

    chore: update demo to show the usage of container for focus trapper case

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  text1: string;
-  text2: string;
-  isCopied1: boolean;
-  isCopied2: boolean;
+    text1: string;
+    text2: string;
+    textModal: string;
+    isCopied1: boolean;
+    isCopied2: boolean;
+    isCopied3: boolean;
+    basic = false;
 }
