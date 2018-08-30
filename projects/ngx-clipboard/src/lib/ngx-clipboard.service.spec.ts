b980f73bb1c15410f9adf35f08ef9fefa0ae22e1
maxisam <maxisam@gmail.com>
2018-08-30 08:48:57 -0500

    style: reformat

--- projects/ngx-clipboard/src/lib/ngx-clipboard.service.spec.ts
+++ projects/ngx-clipboard/src/lib/ngx-clipboard.service.spec.ts
@@ -1,96 +1,67 @@
-import { ClipboardService } from './ngx-clipboard.service';
 import { inject, TestBed } from '@angular/core/testing';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxWindowTokenModule } from 'ngx-window-token';
 
+import { ClipboardService } from './ngx-clipboard.service';
+
 describe('Service: Clipboard', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [BrowserModule, NgxWindowTokenModule],
-      providers: [ClipboardService]
-    });
-  });
-
-  it(
-    'should service work',
-    inject([ClipboardService], (service: ClipboardService) => {
-      expect(service).toBeTruthy();
-    })
-  );
-
-  it(
-    'it is supported',
-    inject([ClipboardService], (service: ClipboardService) => {
-      expect(service.isSupported).toBeTruthy();
-    })
-  );
-
-  describe('check if input is valid', () => {
-    let input: HTMLInputElement;
     beforeEach(() => {
-      input = document.createElement('input');
+        TestBed.configureTestingModule({
+            imports: [BrowserModule, NgxWindowTokenModule],
+            providers: [ClipboardService]
+        });
     });
 
-    it(
-      'input is a valid target',
-      inject([ClipboardService], (service: ClipboardService) => {
-        expect(service.isTargetValid(input)).toBeTruthy();
-      })
-    );
+    it('should service work', inject([ClipboardService], (service: ClipboardService) => {
+        expect(service).toBeTruthy();
+    }));
 
-    it(
-      'input[disabled] is NOT a valid target',
-      inject([ClipboardService], (service: ClipboardService) => {
-        input.setAttribute('disabled', '');
-        expect(() => service.isTargetValid(input)).toThrowError(
-          'Invalid "target" attribute. Please use "readonly" instead of "disabled" attribute'
-        );
-      })
-    );
-  });
+    it('it is supported', inject([ClipboardService], (service: ClipboardService) => {
+        expect(service.isSupported).toBeTruthy();
+    }));
 
-  describe('check if textarea is valid', () => {
-    let ta: HTMLTextAreaElement;
-    beforeEach(() => {
-      ta = document.createElement('textarea');
+    describe('check if input is valid', () => {
+        let input: HTMLInputElement;
+        beforeEach(() => {
+            input = document.createElement('input');
+        });
+
+        it('input is a valid target', inject([ClipboardService], (service: ClipboardService) => {
+            expect(service.isTargetValid(input)).toBeTruthy();
+        }));
+
+        it('input[disabled] is NOT a valid target', inject([ClipboardService], (service: ClipboardService) => {
+            input.setAttribute('disabled', '');
+            expect(() => service.isTargetValid(input)).toThrowError(
+                'Invalid "target" attribute. Please use "readonly" instead of "disabled" attribute'
+            );
+        }));
     });
 
-    it(
-      'textarea is a valid target',
-      inject([ClipboardService], (service: ClipboardService) => {
-        expect(service.isTargetValid(ta)).toBeTruthy();
-      })
-    );
+    describe('check if textarea is valid', () => {
+        let ta: HTMLTextAreaElement;
+        beforeEach(() => {
+            ta = document.createElement('textarea');
+        });
 
-    it(
-      'ta[disabled] is NOT a valid target',
-      inject([ClipboardService], (service: ClipboardService) => {
-        ta.setAttribute('disabled', '');
-        expect(() => service.isTargetValid(ta)).toThrowError(
-          'Invalid "target" attribute. Please use "readonly" instead of "disabled" attribute'
-        );
-      })
-    );
-  });
+        it('textarea is a valid target', inject([ClipboardService], (service: ClipboardService) => {
+            expect(service.isTargetValid(ta)).toBeTruthy();
+        }));
 
-  describe('check if other html element is valid', () => {
-    let div: any;
-    it(
-      'undefined is NOT a valid target',
-      inject([ClipboardService], (service: ClipboardService) => {
-        expect(() => service.isTargetValid(div)).toThrowError(
-          'Target should be input or textarea'
-        );
-      })
-    );
-    it(
-      'div is NOT a valid target',
-      inject([ClipboardService], (service: ClipboardService) => {
-        div = document.createElement('div');
-        expect(() => service.isTargetValid(div)).toThrowError(
-          'Target should be input or textarea'
-        );
-      })
-    );
-  });
+        it('ta[disabled] is NOT a valid target', inject([ClipboardService], (service: ClipboardService) => {
+            ta.setAttribute('disabled', '');
+            expect(() => service.isTargetValid(ta)).toThrowError('Invalid "target" attribute. Please use "readonly" instead of "disabled" attribute');
+        }));
+    });
+
+    describe('check if other html element is valid', () => {
+        let div: any;
+        it('undefined is NOT a valid target', inject([ClipboardService], (service: ClipboardService) => {
+            expect(() => service.isTargetValid(div)).toThrowError('Target should be input or textarea');
+        }));
+        it('div is NOT a valid target', inject([ClipboardService], (service: ClipboardService) => {
+            div = document.createElement('div');
+            expect(() => service.isTargetValid(div)).toThrowError('Target should be input or textarea');
+        }));
+    });
 });
