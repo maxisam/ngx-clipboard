f56cacdafc918b103805df35550ac59bacdec7f0
Matt Lewis <mattlewis92@users.noreply.github.com>
2019-05-06 17:55:47 +0100

    fix: add support for angular 8 (#180)

--- projects/ngx-clipboard/src/lib/ngx-clipboard.service.ts
+++ projects/ngx-clipboard/src/lib/ngx-clipboard.service.ts
@@ -1,5 +1,6 @@
+
 import { Inject, Injectable, Optional } from '@angular/core';
-import { DOCUMENT } from '@angular/platform-browser';
+import { DOCUMENT } from '@angular/common';
 import { WINDOW } from 'ngx-window-token';
 
 // The following code is heavily copy from https://github.com/zenorocha/clipboard.js
