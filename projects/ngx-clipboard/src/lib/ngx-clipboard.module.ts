e6b669fb50125c60f663bffc99907d21a05d3181
Suraj Poddar <surajpoddar16@gmail.com>
2019-05-10 23:03:26 +0530

    feat(clipboard-if-supported): provide a directive for rendering element if copy is supported
    provide a structural directive called ngxClipboardIfSupported that renders the host only if
    isCopySupported returns true

--- projects/ngx-clipboard/src/lib/ngx-clipboard.module.ts
+++ projects/ngx-clipboard/src/lib/ngx-clipboard.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { ClipboardDirective } from './ngx-clipboard.directive';
+import { ClipboardIfSupportedDirective } from './ngx-clipboard-if-supported.directive';
 
 @NgModule({
     imports: [CommonModule],
-    declarations: [ClipboardDirective],
-    exports: [ClipboardDirective]
+    declarations: [ClipboardDirective, ClipboardIfSupportedDirective],
+    exports: [ClipboardDirective, ClipboardIfSupportedDirective]
 })
 export class ClipboardModule {}
