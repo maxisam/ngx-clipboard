ba70b5c4189bb24043bd7c46c081e7bce7ac7fcc
maxisam <maxisam@gmail.com>
2019-11-27 15:42:33 -0600

    fix: for v7 BrowserAnimationsModule  is from @angular/platform-browser/animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule, BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ClarityModule } from '@clr/angular';
 import { ClipboardModule } from 'ngx-clipboard';
 
